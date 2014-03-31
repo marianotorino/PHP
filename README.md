2d3485b0738abc1f57782d60d98f52652228879c
marianotorino <ramiro.antonio@outlook.com>
2014-03-31 15:03:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Proyecto de aplicación web del seminario PHP.
---
+++ inicio.php
@@ -0,0 +1,98 @@
+<html>
+
+<head>
+	<title>Progrmación</title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<h1>Programación</h1>
+	<h3>Grilla de programas del dia 31 de marzo de 2014</h3>
+	<form>Busqueda:<br>
+		Por Categoria:
+		<select name="categoria">
+		<option>Infantiles</option> 
+		<option>Deportivos</option> 
+		<option>Informativos</option> 
+		<option>Comedia</option> 
+		<option>Accion</option>
+		<option>Series</option>  
+		</select>
+		Por Fecha:
+		<select name="fecha">
+		<option>30 de marzo</option> 
+		<option>31 de marzo</option> 
+		<option>1 de abril</option> 
+		<option>2 de abril</option> 
+		<option>3 de abril</option> 
+		<option>4 de abril</option>   
+		</select>
+	
+	
+	</form>
+	<p>Anterior</p>
+	<p>Siguiente</p>
+	<table>
+		<tr>
+			<th>Canal</th>
+			<th>10:00</th>
+			<th>11:00</th>
+			<th>12:00</th>
+			<th>13:00</th>
+			<th>14:00</th>
+			<th>15:00</th>
+		</tr>
+		<tr>
+			<th>Canal 01</th>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+		</tr>
+		<tr>
+			<th>Canal 02</th>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+		</tr>
+		<tr>
+			<th>Canal 03</th>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+		</tr>
+		<tr>
+			<th>Canal 04</th>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+		</tr>
+		<tr>
+			<th>Canal 05</th>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+			<tr>&nbsp;</tr>
+		</tr>
+	
+	
+	</table>
+	
+	
+	
+</body>
+
+</html>
